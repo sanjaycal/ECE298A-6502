71b7da3589f38195a16b36bd88e1166b57fe8e29
sanjaycal <sanjay.calgary@gmail.com>
2025-05-26 16:33:47 -0400

    add a empty cpu class

--- src/project.v
+++ src/project.v
@@ -16,11 +16,14 @@ module tt_um_NES_Emulator (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  reg [7:0] tmp;
+
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  tt_um_6502_module CPU(5, 6 ,uo_out, tmp);
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
