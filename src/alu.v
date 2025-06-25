8217ca5948d1788f7ecce8f131dd6d46f8025037
sanjaycal <sanjay.calgary@gmail.com>
2025-06-25 12:24:41 -0400

    changes to the alu

--- src/alu.v
+++ src/alu.v
@@ -1,4 +1,5 @@
 `include "../inc/alu_ops.vh"
+`include "../inc/status_register.vh"
 
 `default_nettype none
 
@@ -6,19 +7,27 @@ module alu (
     input  wire [2:0]   alu_op,
     input  wire [7:0]   inputA,
     input  wire [7:0]   inputB,
-    output wire [7:0]   ALU_output
+    output wire [7:0]   ALU_output,
+    output wire [7:0]   ALU_flags_output,
 );
 
     reg [7:0] ALU_buffer = 0;
+    reg [7:0] ALU_flags_buffer = 0;
 
     always @(*) begin
         ALU_buffer = 0;
         case(alu_op)
-            `ASL: ALU_buffer = inputA << 1;
+            `ASL: 
+                ALU_flags_buffer[`CARRY_FLAG] = inputA[7];
+                ALU_flags_buffer[`ZERO_FLAG] = ~|inputA;
+                ALU_flags_buffer[`NEGATIVE_FLAG] = inputA[6];
+
+                ALU_buffer = inputA << 1;
             default: ALU_buffer = 0;
         endcase
     end
 
     assign ALU_output = ALU_buffer;
+    assign ALU_flags_output = ALU_flags_buffer;
 
 endmodule
