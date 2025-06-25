4f79f3644b020f1cddae355bc9c70be7a76f4916
sanjaycal <sanjay.calgary@gmail.com>
2025-06-25 13:12:41 -0400

    mixed up left shift and right shift, whoops

--- test/test.py
+++ test/test.py
@@ -117,7 +117,7 @@ async def test_ASL_ZPG(dut):
     dut.uio_in.value = hex_to_num("00")
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 0  # check if we're outputting to 0x0066
-    assert dut.uio_out.value == 34  # check the output
+    assert dut.uio_out.value == 138  # check the output
     assert dut.uio_oe.value == hex_to_num("ff")  # check if we are otuputting
     await ClockCycles(dut.clk, 1)
     assert dut.uio_out.value == 0  # last bit should be 0 for write
